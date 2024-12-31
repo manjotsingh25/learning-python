2060d602c3f99f48de0a70a2785b8fcdfdb09fe0
manjotsingh25 <manjotsingh11945@gmail.com>
2024-12-31 14:52:21 +0530

    chapter4

---
+++ chapter4/list.py
@@ -0,0 +1,4 @@
+list = ["Apple","Orange",25,25.6,"rohan",False]
+print(list[0])
+list[0]= "grapes"# unlike strings list are mutable
+print(list[0])
---
+++ chapter4/list_method.py
@@ -0,0 +1,9 @@
+list = ["Apple","Orange",25,25.6,"rohan",False]
+list.append("manjot") 
+print(list)
+list1 =[32,1,45,88,66,22,44]
+list1.sort()
+list1.reverse()
+list1.insert(5,56)# Insert 56 such that its index in the list is 5
+list1.pop(3)
+print(list1)
